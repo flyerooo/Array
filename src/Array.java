1d10d77ea196608d1047c8233e399519667af73e
flyerooo <wangxiaolong@zhuotaiju.com>
2018-09-13 10:23:12 +0800

    Array add methods

--- src/Array.java
+++ src/Array.java
@@ -1,43 +1,138 @@
-public class Array {
-    private int[] data;
+public class Array<E> {
+    private E[] data;
     private int size;
 
     // 构造函数， 传入数组的容量capacity构造Array
-    public Array(int capacity){
-        data = new int[capacity];
+    public Array(int capacity) {
+        data = (E[]) new Object[capacity];
         size = 0;
     }
 
     // 无参数的构造函数，默认数组的容量capacity=10
-    public Array(){
+    public Array() {
         this(10);
     }
 
-    public int getSize(){
+    public int getSize() {
         return size;
     }
 
-    public int getCapacity(){
+    public int getCapacity() {
         return data.length;
     }
 
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return size == 0;
     }
 
-    public void addLast(int e){
-        if(size == data.length)
-            throw new IllegalArgumentException("AddLast failed. Array is full.");
-        data[size] = e;
-        size ++;
+    public void addLast(E e) {
+        add(size, e);
     }
 
-    public void add(int index, int e){
-        if(size == data.length)
-            throw new IllegalArgumentException("Add failed. Array is full.");
-        if(index <0 || index > size)
+    public void addFirst(E e) {
+        add(0, e);
+    }
+
+    public void add(int index, E e) {
+        if (index < 0 || index > size)
             throw new IllegalArgumentException("Add failed. Require index >= 0 and index <= size.");
 
+        if (size == data.length)
+            resize(2 * data.length);
 
+        for (int i = size - 1; i >= index; i--) {
+            data[i + 1] = data[i];
+        }
+        data[index] = e;
+        size++;
+    }
+
+    E get(int index) {
+        if (index < 0 || index >= size)
+            throw new IllegalArgumentException("Get failed. Index is illegal");
+        return data[index];
+    }
+
+    void set(int index, E e) {
+        if (index < 0 || index >= size)
+            throw new IllegalArgumentException("Get failed. Index is illegal");
+        data[index] = e;
+    }
+
+    // 查找数组中使用有元素e
+    public boolean contains(E e) {
+        for (int i = 0; i < size; i++) {
+            if (data[i].equals(e)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // 查找数组中元素e所在的索引，如果不存在元素e，则返回-1
+    public int find(E e) {
+        for (int i = 0; i < size; i++) {
+            if (data[i] == e) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    // 从数组中删除index位置的元素， 返回删除的元素
+    public E remove(int index) {
+        if (index < 0 || index > size)
+            throw new IllegalArgumentException("Remove failed. Require index >= 0 and index <= size.");
+        E ret = data[index];
+        for (int i = index + 1; i < size; i++) {
+            data[i - 1] = data[i];
+        }
+        size--;
+        data[size] = null;  // loitering objects != member leak
+        if (size == data.length / 2) {
+            resize(data.length / 2);
+        }
+        return ret;
+    }
+
+    // 从数组中删除第一个元素，返回删除的元素
+    public E removeFirst() {
+        return remove(0);
+    }
+
+    // 从数组中删除最后一个元素，返回删除的元素
+    public E removeLast() {
+        return remove(size - 1);
+    }
+
+    // 从数组中删除元素e
+    public void removeElement(E e) {
+        int index = find(e);
+        if (index != -1) {
+            remove(index);
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        res.append(String.format("Array: size= %d , capacity = %d\n", size, data.length));
+        res.append('[');
+        for (int i = 0; i < size; i++) {
+            res.append(data[i]);
+            if (i != size - 1) {
+                res.append(", ");
+            }
+        }
+        res.append(']');
+        return res.toString();
+    }
+
+    private void resize(int newCapacity) {
+        E[] newData = (E[]) new Object[newCapacity];
+        for (int i = 0; i < size; i++) {
+            newData[i] = data[i];
+        }
+        data = newData;
     }
 }
